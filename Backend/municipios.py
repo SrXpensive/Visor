f32e94d5e09e1efb8bf81de424802a6d1fa482a5
Edu <eduardo.caro.lorente@gmail.com>
2024-07-17 08:41:42 +0200

    municipios

--- Backend/municipios.py
+++ Backend/municipios.py
@@ -1,16 +1,41 @@
 import requests
-PROVINCIA = 'VALENCIA'
+from pymongo import MongoClient
+
 # URL del endpoint
 url = 'http://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero/COVCCallejero.svc/json/ObtenerMunicipios?Provincia=VALENCIA' #ObtenerProvincias
+
+# URL de la base de datos
+MONGO = 'mongodb://root:root@localhost'
 
 # Petición GET
 response = requests.get(url)
 
+# Objeto cliente con el que trabajar en la base de datos
+cliente = MongoClient(MONGO)
+
+# Lista de bases de datos existentes en el cliente mongo
+lista_de_bases = cliente.list_database_names()
+
+# Base de datos 'visor'
+db = cliente['visor']
+
+# Lista de colecciones
+lista_de_colecciones = db.list_collection_names()
+
+# Colección 'municipios'
+collection = db['municipios']
+
+# Comprobar que la respuesta de la petición es exitosa mediante el código de respuesta
 if response.status_code == 200:
     try:
+        # Recogemos la respuesta y la parseamos en formato JSON
         data = response.json()
+        # Este contador se utilizará para asignar claves únicas personalizadas
+        cont = 1
+        # Usando d para iterar la respuesta recibida, actualizamos los datos de nuestra base de datos con nuestra clave, y actualizamos el valor del nombre del municipio (si es necesario)
         for d in data['consulta_municipieroResult']['municipiero']['muni']:
-            print(d['nm'])
+            collection.update_one({'_id':cont},{'$set':{'municipio':d['nm']}},upsert= True)
+            cont+=1
     except ValueError:
         print("Error al procesar JSON")
 else:
---
+++ Backend/municipios_test_endpoint.py
@@ -0,0 +1,17 @@
+import requests
+PROVINCIA = 'VALENCIA'
+# URL del endpoint
+url = 'http://ovc.catastro.meh.es/OVCServWeb/OVCWcfCallejero/COVCCallejero.svc/json/ObtenerMunicipios?Provincia=VALENCIA' #ObtenerMunicipios
+
+# Petición GET
+response = requests.get(url)
+
+if response.status_code == 200:
+    try:
+        data = response.json()
+        for d in data['consulta_municipieroResult']['municipiero']['muni']:
+            print(d['nm'])
+    except ValueError:
+        print("Error al procesar JSON")
+else:
+    print(f"Error en la petición: {response.status_code}")
